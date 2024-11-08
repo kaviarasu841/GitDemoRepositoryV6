397805c6d9d7abc96630f0eb91dbc4a60cbec613
kaviarasu1234 <kaviarasu.s@dxc.com>
2024-11-08 16:40:51 +0530

    change 1

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,6 @@
 public class Test1 {
 	public static void main(String args[]) {
+		//change 1
 		String hungry = "yes";
 		if(hungry == "yes" || hungry == "Y")
 			System.out.println("eat something....");
