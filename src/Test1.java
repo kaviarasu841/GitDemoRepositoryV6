40cc8e79e02a84eb3ac5f930846c885a6161424a
kaviarasu1234 <kaviarasu.s@dxc.com>
2024-11-08 16:38:11 +0530

    Added one condition..

--- src/Test1.java
+++ src/Test1.java
@@ -1,7 +1,7 @@
 public class Test1 {
 	public static void main(String args[]) {
 		String hungry = "yes";
-		if(hungry == "yes")
+		if(hungry == "yes" || hungry == "Y")
 			System.out.println("eat something....");
 		else {
 			System.out.println("Do work...");
