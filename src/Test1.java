0201cf539aaca0cce8cc83ec4e930355f13e3aab
Join2Kavi <join2kavi@gmail.com>
2024-11-08 17:18:52 +0530

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -1,8 +1,5 @@
 public class Test1 {
 	public static void main(String args[]) {
-		//change 1
-		//change 2
-		//change 3
 		String hungry = "yes";
 		if(hungry == "yes" || hungry == "Y")
 			System.out.println("eat something....");
